7d0f9822add10a6cae2549f658d94a8abb1c6abd
Michael <emonomics@gmail.com>
2016-12-15 19:41:49 -0500

    added array for a linear path that invaders will take

--- ConsoleApplication1/cSharpTowerDefense/Path.cs
+++ ConsoleApplication1/cSharpTowerDefense/Path.cs
@@ -9,5 +9,16 @@ namespace cSharpTowerDefense
     class Path
     {
 
+        private readonly MapLocation[] _path;
+
+        public Path(MapLocation[] path)
+        {
+            _path = path;
+        }
+
+        public MapLocation GetLocationAt(int pathStep)
+        {
+            return (pathStep < _path.Length) ? _path[pathStep] : null;
+        }
     }
 }
